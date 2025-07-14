e8a2369578b48919e68226d1065a9e21cd51b730
tubaleviao <alvaro.prata.filho@gmail.com>
2025-07-14 03:49:29 -0300

    creating workspaces and making backend runnable

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express'
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response } from 'express'
 
-const app = express();
+const app = express()
 const route = Router()
 
 app.use(express.json())
@@ -10,4 +10,4 @@ route.get('/', (req: Request, res: Response) => {
 })
 app.use(route)
 
-app.listen(3333, () => 'server running on port 3333')
+app.listen(3000, () => console.log('server running on port 3000'))

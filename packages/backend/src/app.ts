cff56e62eaf7afb20d9201800f860b71a85af5ee
Tubaleviao <afilho@miu.edu>
2025-07-19 14:50:30 -0300

    using environment variable file

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,13 +1,14 @@
 import fastify from 'fastify'
 
 const server = fastify()
+const { PORT } = process.env
 
 server.get('/', (req, res) => {
   res.status(200).send({ message: 'hello world with Typescript' })
 })
 
 server.listen(
-  {port: 8080}, 
+  { port: +(PORT ?? 8080) },
   (err, addr) => err ? 
     console.log(err) : 
-    console.log('server running on port 3000', addr))
+    console.log(`server running on`, addr))

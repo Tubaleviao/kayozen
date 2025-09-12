19a2fa03da71239d66090dcfb6795be2e2acdb6c
Tubaleviao <afilho@miu.edu>
2025-09-11 21:32:36 -0300

    adding dynamic google redirectUri

--- utils/google_oauth.ts
+++ utils/google_oauth.ts
@@ -5,8 +5,8 @@ const client = new OAuth2Client({
 	clientSecret: Deno.env.get("GOOGLE_CLIENT_SECRET")!,
 	authorizationEndpointUri: "https://accounts.google.com/o/oauth2/v2/auth",
 	tokenUri: "https://oauth2.googleapis.com/token",
-	redirectUri: `http://localhost:${
-		Deno.env.get("PORT") || "8000"
+	redirectUri: `${Deno.env.get("GOOGLE_REDIRECT")}${
+		Deno.env.get("PORT") ? ":" + Deno.env.get("PORT") : ":8000"
 	}/api/callback`,
 	defaults: {
 		scope: "openid email profile",

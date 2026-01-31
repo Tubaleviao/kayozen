8e4c6418652da1e4aae07ad8f8ab278fdc2056d9
Tubaleviao <alvaro.prata.filho@gmail.com>
2026-01-31 10:43:33 -0300

    updating db script and adding redirection to dashboard

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,8 +1,26 @@
-import { PageProps } from "$fresh/server.ts"
+import { Handlers, PageProps } from "$fresh/server.ts"
 import Footer from "../islands/Footer.tsx"
 import Main from "../islands/Main.tsx"
 import Navbar from "../islands/Navbar.tsx"
 import { KayozenState } from "../utils/interfaces.ts"
+import { getSessionUser } from "../utils/middleware.ts"
+
+export const handler: Handlers = {
+	async GET(req, ctx) {
+		const dbUser = await getSessionUser(req)
+
+		if (dbUser?.email) {
+			return new Response(null, {
+				status: 302,
+				headers: {
+					Location: "/dashboard",
+				},
+			})
+		}
+
+		return await ctx.render()
+	},
+}
 
 export default function Home({ state }: PageProps) {
 	const { dbUser }: Partial<KayozenState> = state
